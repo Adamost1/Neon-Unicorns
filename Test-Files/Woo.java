4586fae77e77b4e0badd46b578e104c4e0568537
Alvin Ye <aye4@stuy.edu>
2018-01-09 23:02:48 -0500

    added game over system and if occupied space fail system

--- Test-Files/Woo.java
+++ Test-Files/Woo.java
@@ -36,21 +36,21 @@ public class Woo{
     }
 
     public static boolean isEmpty(int row, int column) {
-	return Board[row - 1][column - 1].equals(" ");
+	return Board[row][column].equals(" ");
     }
 
-    public static void AIDiffE() {
+    public static void AIDiffE() { //AI strategy for easy mode
 	int x = (int)(Math.random() * (Board.length) + 1);
 	int y = (int)(Math.random() * (Board[0].length) + 1);
-	while (Board[x - 1][y - 1] != " ") {
+	while ( !(isEmpty(x - 1, y - 1)) ) {
 	    x = (int)(Math.random() * (Board.length) + 1);
 	    y = (int)(Math.random() * (Board[0].length) + 1);
-	    System.out.println(x + "\t" + y);
+	    System.out.println("===========debugging==========\n" + x + "\t" + y + "\n===================");
 	}
 	Board[x - 1][y - 1] = "O";
     }
     
-    public static int PinputR() {
+    public static int PinputR() { //reads the user input for row and returns the input
 	System.out.print("what row? ");
 	int row = Keyboard.readInt();
 	while (row != 1 && row != 2 && row != 3) {
@@ -61,7 +61,7 @@ public class Woo{
 	}
 	return row;
     }
-    public static int PinputC() {
+    public static int PinputC() { //reads the user input for column and returns the input
 	System.out.print("what column? ");
 	int column = Keyboard.readInt();
 	while (column != 1 && column != 2 && column != 3) {
@@ -72,7 +72,42 @@ public class Woo{
 	}
 	return column;
     }
+
+    public static boolean isWin(){
+    	for(String[] array: Board){
+
+    			if(array[0].equals(array[1]) && array[1].equals(array[2]) ){
+    				if(array[0].equals(" ")){
+    					return false;
+    				}
+    				else{
+    				return true;
+    			}
+
+    			}
+    		
+    	}
+
+
+    	for(int c = 0; c < Board.length; c ++){
+
+    			if(Board[0][c].equals(Board[1][c]) && Board[1][c].equals(Board[2][c]) ){
+    				if(Board[0][c].equals(" ")){
+    					return false;
+    				}
+    				else{
+    				return true;
+    			}
+    			}
+    		
+    	}
+
+    	return false;
+    }
+
     public static void main(String[] args) {
+
+
 	System.out.println("Choose your difficulty");
 	System.out.println("1. Easy\n2. Medium\n3. Hard");
 	int difficulty = Keyboard.readInt();
@@ -84,18 +119,31 @@ public class Woo{
 	    difficulty = Keyboard.readInt();
 	    
 	}
-	
+
+
 	Woo tic = new Woo();
 	System.out.println(tic);
-	for (int i = 0; i < 5; i++) {
+	while (isWin() == false ) {
 	    int row = PinputR();
 	    int column = PinputC();
+
+	    while(  isEmpty(row - 1, column - 1) == false ){    //prints message if the player chooses coordinates of an occupied spot
+	    	System.out.println( "Pick an empty spot!");
+	    	row = PinputR();
+	    	column = PinputC();
+	    }
+
 	    playerInput(row,column);
+	    System.out.println("Your Move:");
+	    System.out.print(tic);
+	    if(isWin()){
+	    	break;
+	    }
 	    AIDiffE();
+	    System.out.println("AI Move:");
 	    System.out.print(tic);
 	}
-	System.out.println(isEmpty(1,1));
-
+	System.out.println("Game Over");
 
     }
 }
