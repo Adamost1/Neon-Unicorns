80def18584d4141827e4016bea7ec00b62c49f3c
Alvin Ye <aye4@stuy.edu>
2018-01-15 15:42:34 -0500

    .

--- Test-Files/Woo.java
+++ Test-Files/Woo.java
@@ -119,10 +119,9 @@ public class Woo{
 	
 
 	AIDiffE(); //resorts to AIDiffE(), or random moves when there are not two elements in a row or column
+    }//end method
 
 
-
-    }
     public static void AIDiffH() {
 	if (isEmpty(1,1)) {
 	    Board[1][1] = "O";
@@ -157,71 +156,7 @@ public class Woo{
 	    }
 	}
     }
-    public static void HelperAIDiffH() {
-	for(int r = 0; r < Board.length; r ++){//checks row by row for two in a row
-	    
-	    if(
-	       (Board[r][0].equals(Board[r][1]) && !(Board[r][0].equals(Board[r][2])) && !(Board[r][0].equals(" ")) ) ||
-	       (Board[r][0].equals(Board[r][2]) && !(Board[r][0].equals(Board[r][1])) && !(Board[r][0].equals(" ")) ) ||
-	       (Board[r][1].equals(Board[r][2]) && !(Board[r][1].equals(Board[r][0])) && !(Board[r][1].equals(" ")) )     			
-	       )
-    		{
-		    for(int c = 0; c < Board.length; c ++){
-			if(Board[r][c].equals(" ")){
-			    Board[r][c] = "O";
-			    return;
-			}
-		    }
-    		}//close if
-	}//close for
-	
-    	for(int c = 0; c < Board.length; c ++){//checks column by column for two in a column
-	    
-	    if(
-	       (Board[0][c].equals(Board[1][c]) && !(Board[0][c].equals(Board[2][c])) && !(Board[0][c].equals(" ")) ) ||
-	       (Board[0][c].equals(Board[2][c]) && !(Board[0][c].equals(Board[1][c])) && !(Board[0][c].equals(" ")) ) ||
-	       (Board[1][c].equals(Board[2][c]) && !(Board[1][c].equals(Board[0][c])) && !(Board[1][c].equals(" ")) )     			
-	       )
-    		{
-		    for(int r = 0; r < Board.length; r ++){
-			if(Board[r][c].equals(" ")){
-			    Board[r][c] = "O";
-			    return;
-			}
-		    }
-    		}//close if
-	}//close for
-	
-	if ( //checks the first diagonal, going from top left to bottom right, for 2 elements 
-	    (Board[0][0].equals(Board[1][1]) && !(Board[0][0].equals(Board[2][2])) && !(Board[0][0].equals(" ")) ) ||
-	    (Board[0][0].equals(Board[2][2]) && !(Board[0][0].equals(Board[1][1])) && !(Board[0][0].equals(" ")) ) ||
-	    (Board[1][1].equals(Board[2][2]) && !(Board[1][1].equals(Board[0][0])) && !(Board[1][1].equals(" ")) )
-	     ) 
-	    {
-		for(int i = 0; i < Board.length; i ++) {
-		    if (Board[i][i].equals(" ")) {
-			Board[i][i] = "O";
-			return;
-		    }
-		}
-	    }// closes if
-	
-	if (		
-	    (Board[0][2].equals(Board[1][1]) && !(Board[0][2].equals(Board[2][0])) && !(Board[0][2].equals(" ")) ) ||
-	    (Board[0][2].equals(Board[2][0]) && !(Board[0][2].equals(Board[1][1])) && !(Board[0][2].equals(" ")) ) ||
-	    (Board[1][1].equals(Board[2][0]) && !(Board[1][1].equals(Board[0][2])) && !(Board[1][1].equals(" ")) )
-			)
-	    {
-		for(int i = 0; i < Board.length; i ++){
-		    if (Board[i][2 - i].equals(" ")){
-			Board[i][2 - i] = "O";
-			return;
-		    }
-		}
-	    }
-	
-	
-    }
+
     public static int PinputR() { //reads the user input for row and returns the input
 	System.out.print("what row? ");
 	int row = Keyboard.readInt();
